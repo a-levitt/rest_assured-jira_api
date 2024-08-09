4df0824ca32bbbf17a44ac4d1a1e097fc8c1ea84
a-levitt <artemis.levitt@gmail.com>
2024-08-09 15:43:20 +0200

    Solving complex queries from json with pojo methods

--- src/test/java/alevitt/OAuth2Test.java
+++ src/test/java/alevitt/OAuth2Test.java
@@ -6,6 +6,7 @@ import io.restassured.RestAssured;
 import io.restassured.path.json.JsonPath;
 import pojo.Api;
 import pojo.GetCourse;
+import pojo.WebAutomation;
 
 import java.util.Arrays;
 import java.util.List;
@@ -54,14 +55,15 @@ public class OAuth2Test {
         System.out.println(jsonObject.getLinkedIn());
         //System.out.println(jsonObject.getCourses().getApi().get(1).getCourseTitle());
 
-        List<Api> apiCourses = jsonObject.getCourses().getApi();
-        for (int i = 0; i < apiCourses.size(); i++) {
-            if (apiCourses.get(i).getCourseTitle().equalsIgnoreCase("SoapUI Webservices testing")) {
-                System.out.println(apiCourses.get(i).getCourseTitle());
-                System.out.println(apiCourses.get(i).getPrice());
-            }
+        int webAutomationCoursesPriceSum = 0;
+        List<WebAutomation> webAutomationCourses = jsonObject.getCourses().getWebAutomation();
+        System.out.println("Web automation courses:");
+        for (int i = 0; i < webAutomationCourses.size(); i++) {
+                System.out.println(webAutomationCourses.get(i).getCourseTitle() +
+                        ", costs " +        webAutomationCourses.get(i).getPrice() + "$");
+                webAutomationCoursesPriceSum += webAutomationCourses.get(i).getPrice();
         }
+        System.out.println("Price of all web automation courses: " + webAutomationCoursesPriceSum + "$");
     }
-
 
 }
